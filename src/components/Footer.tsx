acd63a82c371f01325a17a4abe25baf76199084b
Omar Campos <omarjc-1997@hotmail.com>
2022-09-21 15:50:10 -0500

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,19 @@
 export const Footer = () => {
-  return <div>Footer</div>
+  const socialLinks = ["instagram", "youtube", "facebook", "twitter"]
+
+  return (
+    <div className="bg-white p-5 text-gray-700 flex justify-center ">
+      <div className="flex flex-col justify-center items-center">
+        <span className="text-lg mb-3">Síguenos en nuestra redes sociales</span>
+
+        <div className="grid grid-cols-4 gap-5">
+          {socialLinks.map((social) => (
+            <a href={`https://www.${social}.com/`} target="blank" key={social}>
+              <i className={`text-3xl fa-brands fa-${social}`} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
